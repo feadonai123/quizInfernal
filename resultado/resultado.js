9eb597a9d893eaafaee03e579c61f22dff0067a4
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2025-12-10 10:04:52 -0300

    resultados

--- resultado/resultado.js
+++ resultado/resultado.js
@@ -11,9 +11,17 @@ const picKeys = Object.keys(pics);
 
 const resultadoFake2 = document.getElementById('fakeResultado2');
 const resultadoFake1 = document.getElementById('fakeResultado1');
+hell(() => {
 resultadoFake1.addEventListener('mouseover', () => moveFake(resultadoFake1));
 resultadoFake2.addEventListener('click', () => moveFake(resultadoFake2));
-
+});
+function getResultValues(){
+    let somaResult = 0;
+    somaResult += +CookieStorage.get('question-1-answer');
+    somaResult += +CookieStorage.get('question-2-answer');
+     somaResult += +CookieStorage.get('question-3-answer');
+     return somaResult;
+  }
 function setupResult() {
   const img = document.createElement('img');
   img.id = 'imagemResultado';
@@ -27,10 +35,15 @@ function setupResult() {
     resultadoReal.style.display = 'block';
   }, 15);
 
-  const a = Math.floor(Math.random() * picKeys.length);
+  let a = getResultValues();
+  console.log("Result",a)
+  if(a > 4){
+a = a%4;
+  } 
   const selectedPic = picKeys[a];
-
+  
   textoRes.after(img);
+  
   resultadoReal.addEventListener('click', function () { getResult(resultadoReal, textoRes, img, selectedPic); });
 
 
@@ -45,10 +58,13 @@ function moveFake(button) {
 }
 function getResult(resultadoReal, textoRes, img, selectedPic) {
   if (selectedPic !== 'Falha') {
+    hell(() => {
     resultadoReal.textContent = "Você tem certeza que quer ver seu resultado?";
-
+      });
     resultadoReal.addEventListener('click', function () {
+      hell(() => {
       resultadoReal.textContent = "Clica de Novo se REALMENTE quer o resultado";
+      });
       resultadoReal.addEventListener('click', function () {
         img.src = pics[selectedPic];
         textoRes.textContent = 'Parabéns você é um ' + selectedPic;
